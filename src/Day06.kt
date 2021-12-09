43ae13137d35443bcb26a6f90b863f1d57572bf3
Philip Nagler-Frank <philip@nagler.world>
2021-12-09 20:40:08 +0100

    Day 06 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,44 +1,41 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val state: MutableList<Int> = input[0].split(",")
-            .map { it.toInt() }
-            .toMutableList()
+    fun fishGrowth(input: List<String>, days: Int): Long {
+        var state = mutableMapOf<Int, Long>()
+        (0..8).forEach { state[it] = 0 }
 
-        (0 until 80).forEach { _ ->
-            var births = 0
-            (0 until state.size).forEach { index ->
-                state[index] = if(state[index] == 0) {
-                    births++
-                    6
-                } else {
-                    state[index] - 1
-                }
+        input[0].split(",")
+            .map { it.toInt() }
+            .forEach {
+                state.compute(it) { _, count -> count!! + 1 }
             }
-            (0 until  births).forEach {
-                state.add(8)
+
+        (0 until days).forEach { _ ->
+            val newState = mutableMapOf<Int, Long>()
+            newState[8] = state[0]!!
+            (7 downTo 0).forEach {
+                newState[it] = state[it + 1]!!
             }
+            newState[6] = newState[6]!! + state[0]!!
+
+            state = newState
         }
 
-        return state.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
+        return state.values.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
 
-    val testOutput1 = part1(testInput)
+    val testOutput1 = fishGrowth(testInput, 80)
     println("test output1: $testOutput1")
-    check(testOutput1 == 5934)
+    check(testOutput1 == 5934L)
 
-    val testOutput2 = part2(testInput)
+    val testOutput2 = fishGrowth(testInput, 256)
     println("test output2: $testOutput2")
-    //check(testOutput2 == -1)
+    check(testOutput2 == 26984457539)
 
     val input = readInput("Day06")
-    println("output part1: ${part1(input)}")
-    println("output part2: ${part2(input)}")
+    println("output part1: ${fishGrowth(input, 80)}")
+    println("output part2: ${fishGrowth(input, 256)}")
 }
