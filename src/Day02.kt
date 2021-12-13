79ec2df42ba1e9a7c7540e7a7578e0598157c759
Philip Nagler-Frank <philip@nagler.world>
2021-12-13 14:41:55 +0100

    Day 02 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -21,14 +21,37 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val result: Triple<Int, Int, Int> = input
+            .fold(Triple(0, 0, 0)) { (position, depth, aim), step ->
+                val (direction, countStr) = step.split(" ")
+                val count = Integer.parseInt(countStr)
+                Triple(
+                    when (direction) {
+                        "forward" -> position + count
+                        else -> position
+                    },
+                    when (direction) {
+                        "forward" -> depth + aim * count
+                        else -> depth
+                    },
+                    when(direction) {
+                        "down" -> aim + count
+                        "up" -> aim - count
+                        else -> aim
+                    }
+                )
+            }
+        return result.first * result.second
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    val testOutput = part1(testInput)
-    println("test output: $testOutput")
-    check(testOutput == 150)
+    val testOutput1 = part1(testInput)
+    println("test output 1: $testOutput1")
+    check(testOutput1 == 150)
+    val testOutput2 = part2(testInput)
+    println("test output: $testOutput2")
+    check(testOutput2 == 900)
 
     val input = readInput("Day02")
     println("output part1: ${part1(input)}")
