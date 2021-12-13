bb70d94a04873ee3f2032d8a42e34fdb0a27102b
Philip Nagler-Frank <philip@nagler.world>
2021-12-13 18:01:41 +0100

    Day 03 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,6 +1,7 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        val setBitCounts = IntArray(input[0].length)
+
+    fun countBits(input: Iterable<String>): IntArray {
+        val setBitCounts = IntArray(input.first().length)
         for (line in input) {
             line.toCharArray().forEachIndexed { index, c ->
                 if (c == '1') {
@@ -8,6 +9,11 @@ fun main() {
                 }
             }
         }
+        return setBitCounts
+    }
+
+    fun part1(input: List<String>): Int {
+        val setBitCounts = countBits(input)
 
         val gamma = setBitCounts.map {
             if (it > input.size / 2) {
@@ -29,14 +35,37 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val candidatesOxygen = input.toMutableSet()
+        val candidatesScrubber = input.toMutableSet()
+
+        for(i in input.indices) {
+            if (candidatesOxygen.size > 1) {
+                val setBitCounts = countBits(candidatesOxygen)
+                val desiredBit = if(setBitCounts[i].toFloat() / candidatesOxygen.size.toFloat() >= 0.5f) '1' else '0'
+                candidatesOxygen.retainAll { it[i] == desiredBit }
+            }
+            if (candidatesScrubber.size > 1) {
+                val setBitCounts = countBits(candidatesScrubber)
+                val desiredBit = if(setBitCounts[i].toFloat() / candidatesScrubber.size.toFloat() < 0.5f) '1' else '0'
+                candidatesScrubber.retainAll { it[i] == desiredBit }
+            }
+        }
+
+        val oxy = candidatesOxygen.also { check(it.size == 1)}.first().toInt(2)
+        val scr = candidatesScrubber.also { check(it.size == 1)}.first().toInt(2)
+
+        return oxy * scr
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    val testOutput = part1(testInput)
-    println("test output: $testOutput")
-    check(testOutput == 198)
+    val testOutput1 = part1(testInput)
+    println("test output: $testOutput1")
+    check(testOutput1 == 198)
+    val testOutput2 = part2(testInput)
+    println("test output: $testOutput2")
+    check(testOutput2 == 230)
 
     val input = readInput("Day03")
     println("output part1: ${part1(input)}")
